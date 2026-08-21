1662cce1350c403b5412b4d01991bdaadf1e022a
Dinesh Kilaru <dineshkilaru08@gmail.com>
2026-08-21 19:36:49 +0530

    Change base directory for model files

--- Career_and_salary_prediction/career_salary_app_.py
+++ Career_and_salary_prediction/career_salary_app_.py
@@ -6,7 +6,7 @@ import joblib
 import streamlit as st
 from scipy.sparse import hstack
 
-BASE_DIR_DEFAULT = "C:/Users/dines/OneDrive/Desktop/ry prediction"
+BASE_DIR_DEFAULT = "Models"
 
 FILES = {
     "career_model": "career_recommendation_model.pkl",
