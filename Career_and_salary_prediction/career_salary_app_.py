a4ac549d9b79c7d9ddce0d4198585cf03f9e8f94
Dinesh Kilaru <dineshkilaru08@gmail.com>
2026-08-25 20:30:03 +0530

    Update career_salary_app_.py

--- Career_and_salary_prediction/career_salary_app_.py
+++ Career_and_salary_prediction/career_salary_app_.py
@@ -13,7 +13,6 @@ import streamlit as st
 import plotly.graph_objects as go
 from scipy.sparse import hstack, csr_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
-from google import genai
 from urllib.parse import quote
 import io
 
